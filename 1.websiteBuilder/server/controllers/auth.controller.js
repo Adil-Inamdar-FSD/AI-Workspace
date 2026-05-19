6764a28bcfea10c92827d8aa55237a73a8b1ffbd
Adil Inamdar <adilinamdar617@gmail.com>
2026-05-19 09:32:56 +0530

    Update auth.controller.js

--- 1.websiteBuilder/server/controllers/auth.controller.js
+++ 1.websiteBuilder/server/controllers/auth.controller.js
@@ -29,13 +29,12 @@ export const googleAuth = async (req, res) => {
     });
 
     // Check environment
-    const isProduction = process.env.NODE_ENV === "production";
-
+  
     // Set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: isProduction, // false for localhost
-      sameSite: isProduction ? "none" : "lax",
+      secure: true, // false for localhost
+      sameSite:  "none" ,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -57,12 +56,12 @@ export const googleAuth = async (req, res) => {
 
 export const logOut = async (req, res) => {
   try {
-    const isProduction = process.env.NODE_ENV === "production";
+   
 
     res.clearCookie("token", {
       httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction ? "none" : "lax",
+      secure: true,
+      sameSite:   "none" ,
     });
 
     return res.status(200).json({
