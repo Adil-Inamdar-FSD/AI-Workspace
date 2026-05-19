faef81fa8ca3dd1744c554af2cef9414acd770ed
Adil Inamdar <adilinamdar617@gmail.com>
2026-05-19 12:32:19 +0530

    Update auth.controller.js

--- 1.websiteBuilder/server/controllers/auth.controller.js
+++ 1.websiteBuilder/server/controllers/auth.controller.js
@@ -35,10 +35,9 @@ res.cookie("token", token, {
   httpOnly: true,
   secure: true,
   sameSite: "none",
+  path: "/",
   maxAge: 7 * 24 * 60 * 60 * 1000,
-  domain: ".onrender.com",
 });
-
     return res.status(200).json({
       success: true,
       message: "Login successful",
