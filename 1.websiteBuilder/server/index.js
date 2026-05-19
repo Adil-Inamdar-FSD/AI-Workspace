30662f926d246c28b8e8c32fff5fd4cc300a0f2c
Adil Inamdar <adilinamdar617@gmail.com>
2026-05-19 09:43:09 +0530

    Update index.js

--- 1.websiteBuilder/server/index.js
+++ 1.websiteBuilder/server/index.js
@@ -24,7 +24,7 @@ const port = process.env.PORT || 5000;
 // CORRECT CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://genweb-ai-client.onrender.com",
     credentials: true,
   }),
 );
