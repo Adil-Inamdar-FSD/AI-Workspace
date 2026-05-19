f7eec4c075ae1f150e383ecda8f05c3a885e9900
Adil Inamdar <adilinamdar617@gmail.com>
2026-05-19 12:49:38 +0530

    Update index.js

--- 1.websiteBuilder/server/index.js
+++ 1.websiteBuilder/server/index.js
@@ -34,7 +34,10 @@ app.use(
     credentials: true,
   }),
 );
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
