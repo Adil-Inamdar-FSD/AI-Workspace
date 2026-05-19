cc61aa169ae68d6f7b9a8e2478ab44effe8dc50e
Adil Inamdar <adilinamdar617@gmail.com>
2026-05-19 14:09:30 +0530

    Update useGetCurrentUser.jsx

--- 1.websiteBuilder/client/src/hooks/useGetCurrentUser.jsx
+++ 1.websiteBuilder/client/src/hooks/useGetCurrentUser.jsx
@@ -1,7 +1,5 @@
-import React from "react";
 import { useEffect } from "react";
 import axios from "axios";
-import { serverUrl } from "../config/config";
 import { useDispatch } from "react-redux";
 import { setUserData } from "../redux/userSlice";
 
@@ -11,18 +9,19 @@ function useGetCurrentUser() {
   useEffect(() => {
     const getCurrentUser = async () => {
       try {
-        const result = await axios.get(`${serverUrl}/api/user/me`, {
-          withCredentials: true,
-        });
+        const result = await axios.get(
+          `${import.meta.env.VITE_SERVER_URL}/api/user/me`,
+          { withCredentials: true }
+        );
 
         dispatch(setUserData(result.data));
       } catch (error) {
-        console.log(error);
+        console.log("User fetch error:", error.response?.data || error.message);
       }
     };
 
     getCurrentUser();
-  }, []);
+  }, [dispatch]);
 }
 
 export default useGetCurrentUser;
