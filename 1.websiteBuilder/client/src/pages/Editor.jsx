e2ba6aa1fbbb1df5adaf81a97c3a6f81cffd719b
Adil Inamdar <adilinamdar617@gmail.com>
2026-05-19 13:12:14 +0530

    Update Editor.jsx

--- 1.websiteBuilder/client/src/pages/Editor.jsx
+++ 1.websiteBuilder/client/src/pages/Editor.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { serverUrl } from "../App";
+import { serverUrl } from "../config/config";
 import {
   ArrowLeft,
   Code2,
