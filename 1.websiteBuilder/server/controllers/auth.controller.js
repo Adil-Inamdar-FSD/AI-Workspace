95ffa32fdded66eb4c56a94315a540eeeb4b0eff
Adil Inamdar <adilinamdar617@gmail.com>
2026-05-19 12:20:58 +0530

    Update auth.controller.js

--- 1.websiteBuilder/server/controllers/auth.controller.js
+++ 1.websiteBuilder/server/controllers/auth.controller.js
@@ -31,12 +31,13 @@ export const googleAuth = async (req, res) => {
     // Check environment
   
     // Set cookie
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true, // false for localhost
-      sameSite:  "none" ,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  domain: ".onrender.com",
+});
 
     return res.status(200).json({
       success: true,
